831da9acefa14a810cbde10a3680038fe6c07ba5
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-03-15 21:17:15 +0200

    adding file and folder multi search

--- fileSearch.py
+++ fileSearch.py
@@ -1,33 +1,52 @@
 import argparse
 import os
+import re
 
 #create argparse object
 argparseObject = argparse.ArgumentParser(description="File and Folder search")
-argparseObject.add_argument("-f","--file",help="file name to search",nargs="?",type=str)
-argparseObject.add_argument("-d","--dir",help="folder name to search", nargs="?",type=str)
+argparseObject.add_argument("-f","--file",help="file name to search",nargs="+",type=str)
+argparseObject.add_argument("-d","--dir",help="folder name to search", nargs="+",type=str)
 argparseObject.add_argument("-p","--path",help="path to start the search from",default=".",type=str)
+groupSelect = argparseObject.add_mutually_exclusive_group()
+groupSelect.add_argument("-e","--end",help="Search file that ends with certain characters",action="store_true")
+groupSelect.add_argument("-s","--start",help="Search file that start with certain characters",action="store_true")
+groupSelect.add_argument("-c","--contain",help="Search file that Contain  certain characters",action="store_true")
 value = argparseObject.parse_args()
 
 
-def fileSearch(pathToSearchFrom,fileToSearch): 
-    walkDataObject = os.walk(pathToSearchFrom)
-    for path,_,files in walkDataObject:
-        for file in files:
-            if file == fileToSearch:
-                print(str(path)+"\\"+str(file))
 
-def dirSearch(pathToSearchFrom,dirToSearch):
-    walkDataObject = os.walk(pathToSearchFrom)
-    for path,dirs,_ in walkDataObject:
-        for dir in dirs:
-            if dir == dirToSearch:
-                 print(str(path)+"\\"+str(dir))
+def fileSearchMatch(pathToSearchFrom,fileToSearchList): 
+    for fileToSearch in fileToSearchList:
+        walkDataObject = os.walk(pathToSearchFrom)
+        for path,_,files in walkDataObject:
+            for file in files:
+                if file == fileToSearch:
+                    print(str(path)+"\\"+str(file))
+
+def fileSearchMatchEnd(pathToSearchFrom,fileToSearchList):
+   pass
+
+def fileSearchMatchStart(pathToSearchFrom,fileToSearchList):
+    pass
+
+def fileSearchMatchContain(pathToSearchFrom,fileToSearchList):
+    pass
+
+def dirSearchMatch(pathToSearchFrom,dirToSearchList):
+    for dirToSearch in dirToSearchList:
+        walkDataObject = os.walk(pathToSearchFrom)
+        for path,dirs,_ in walkDataObject:
+            for dir in dirs:
+                if dir == dirToSearch:
+                    print(str(path)+"\\"+str(dir))
+
+
 
 if __name__ == "__main__":
     print("\n_______________ Files _______________")
     if value.file != None:
-        fileSearch(value.path,value.file)
+       fileSearchMatch(value.path,value.file)
     
     print("\n_______________ Folders ______________")
     if value.dir != None:
-        dirSearch(value.path,value.dir)
+        dirSearchMatch(value.path,value.dir)
